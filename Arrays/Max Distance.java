2dada343e4389585ec0fab1d5282576b6895d557
aniruddhaalek <anirudha.alek@gmail.com>
2018-10-09 19:49:59 +0530

    Update Max Distance.java

--- Arrays/Max Distance.java
+++ Arrays/Max Distance.java
@@ -10,23 +10,36 @@ Output : 2
 for the pair (3, 4)
 */
 
-class Solution {
-        int maximumGap(const vector<int> &num) {
-            if (num.size() == 0) return -1;
-            if (num.size() == 1) return 0;
-            vector<pair<int, int> > toSort; 
-            for (int i = 0; i < num.size(); i++) {
-                toSort.push_back(make_pair(num[i], i));
-            }
-            sort(toSort.begin(), toSort.end());
-            int len = toSort.size();
-            int maxIndex = toSort[len - 1].second;
-            int ans = 0;
-            for (int i = len - 2; i >= 0; i--) {
-                ans = max(ans, maxIndex - toSort[i].second);
-                maxIndex = max(maxIndex, toSort[i].second);
-            }
-            
-        }
-        return ans;
+public class Solution {
+	// DO NOT MODIFY THE LIST
+	public int maximumGap(final List<Integer> a) {
+	    if (a == null || a.size() == 0) return -1;
+
+	    boolean[] isMin = new boolean[a.size()];
+	    int min = a.get(0);
+	    int maxDistance = 0;
+		int i = a.size()-1;
+    	int j = a.size()-1;
+    
+	    for(i = 0; i < a.size(); i++){
+	        if (a.get(i) <= min){
+	            min = a.get(i);
+	            isMin[i] = true;
+	        }
+	    }
+	    i--;
+
+	    while (i >= 0){
+	    	if (!isMin[i]) i--;
+	    	else {
+	    		while (a.get(i) > a.get(j) && j > i){
+		    		j--;
+		    	}
+		    	if ( j - i > maxDistance) maxDistance = j - i;
+		    	i--;
+	    	}
+	    }
+
+	    return maxDistance;
+	}
 }
